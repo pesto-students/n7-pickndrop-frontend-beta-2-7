7b3e087ecbeb6bf17bcbdfa8c302348a97707538
Akash Kumar <akashkumar@Akashs-MacBook-Pro.local>
2021-10-07 22:48:04 +0530

    change actual backend url

--- src/services/apiurl.js
+++ src/services/apiurl.js
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.API_URL
-    : "https://pickndrop-backend.herokuapp.com";
+export const BASE_URL = "https://pickndrop-backend.herokuapp.com";
